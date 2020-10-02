19d802a140c894a1150d55eb8427c7da53413d87
Vaniahj <vaniahj@hotmail.com>
2020-10-01 22:27:49 -0300

    Configurei acesso ao banco

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-3-216-129-140.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'dffaovtdpb78m5'),
+            'username' => env('DB_USERNAME', 'bgqqujzesqrqci'),
+            'password' => env('DB_PASSWORD', '303a7a9c9d0e6e95b5cddfd0d3144335daa258d2236e588d38d0dbf0be112d07'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
