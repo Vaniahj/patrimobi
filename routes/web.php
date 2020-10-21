73b9441fa688bcc3b342c5a0d6115465deff793f
Vaniahj <vaniahj@hotmail.com>
2020-10-20 22:56:33 -0300

    rota que adiciona e lista usuarios no banco de dados

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/userlist', function () {
+    $umUsuarioQualquer = new User;
+    $umUsuarioQualquer->name = 'aUserName';
+
+    $timestamp = date("Y-m-d-h-i-sa");
+    $umUsuarioQualquer->email = "adsf@asdf.com".$timestamp;
+    
+    $umUsuarioQualquer->password = '123';
+    $umUsuarioQualquer->save();
+    return User::all();
+});
